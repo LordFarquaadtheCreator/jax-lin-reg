4d0c12ebdb9697884c6c5b6fa9fa1b011235b074
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-12 19:39:22 -0400

    cleanup

--- get_intercept.py
+++ get_intercept.py
@@ -1,12 +1,13 @@
-import jax.numpy as jnp
-from jax import grad, jit
-from get_slope import get_slope
+from timer import timer
 
 
+@timer
 def get_intercept(x, y, b):
-	'''a = y' - bx' linear regression'''
-	mean_x: float = jnp.mean(x)
-	mean_y: float = jnp.mean(y)
-	a: float = mean_y - (b * mean_x)
-    
-	return a
+    import jax.numpy as jnp
+
+    """a = y' - bx' linear regression"""
+    mean_x: float = jnp.mean(x)
+    mean_y: float = jnp.mean(y)
+    a: float = mean_y - (b * mean_x)
+
+    return a
